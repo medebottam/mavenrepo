22b5c58a8431d68abe20768417ea8a04253bb20f
Debottam <medebottam@gmail.com>
2018-06-04 12:44:48 +0000

    updated latest

--- src/main/java/com/sampledemo/App.java
+++ src/main/java/com/sampledemo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Mindtree!!" );
+        System.out.println( "Hello Mindtree 1234!!" );
     }
 }
