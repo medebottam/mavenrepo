5bb8ef4c1c70151f5c3561833cc32ead16e65a02
Debottam <medebottam@gmail.com>
2018-06-04 13:07:51 +0000

    minor updates

--- src/main/java/com/sampledemo/App.java
+++ src/main/java/com/sampledemo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Mindtree Minds!! Welcome to DevOps!!" );
+        System.out.println( "Hello Mindtree Minds!! Welcome to DevOps session!!" );
     }
 }
