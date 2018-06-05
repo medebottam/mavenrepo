4de8ea4ff83a4d7d6e1d2f6c0003acb099c52f93
Debottam <medebottam@gmail.com>
2018-06-05 06:01:13 +0000

    minor updates

--- src/main/java/com/sampledemo/App.java
+++ src/main/java/com/sampledemo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Mindtree Minds!! this is a test number 1" );
+        System.out.println( "Hello Mindtree Minds!!" );
     }
 }
